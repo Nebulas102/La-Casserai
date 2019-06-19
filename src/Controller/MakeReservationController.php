757d88691d7288c7a973cdc61b9e139690028add
Mike Braber <nokinsu@gmail.com>
2019-06-19 22:00:28 +0200

    update

--- src/Controller/MakeReservationController.php
+++ src/Controller/MakeReservationController.php
@@ -32,4 +32,63 @@ class MakeReservationController extends AbstractController
             'form' => $form->createview(),
         ]);
     }
+
+    /**
+     * @Route("result/checkindate={checkindate}&checkoutdate={checkoutdate}", name="result_room")
+     */
+    public function roomSearch(RoomRepository $roomRepository, Request $request, $checkoutDate, $checkinDate)
+    { 
+        return $this->render('make_reservation/result.html.twig' ,[
+            'rooms' => $roomRepository->roomOccupied($checkinDate, $checkoutDate),
+            'checkindate' => $checkinDate,
+            'checkoutdate' => $checkoutDate,
+        ]);
+    }
+
+    /**
+     * @Route("/result/checkindate={checkindate}&checkoutdate={checkoutdate}/view/{id}", name="view")
+     */
+    public function overview($checkinDate, $checkoutDate, RoomRepository $roomRepository, Request $request, $id)  : Response{
+        $dateCheckin = date_create($checkinDate);
+        $dateCheckout = date_create($checkoutDate);
+
+        $total_days = date_diff($dateCheckin, $dateCheckout)->format("%d");
+        $total_price = $roomRepository->createQueryBuilder('u')->select('u.price')->where('id', $id);
+        return $this->render('default/overview.html.twig', [
+            'rooms' => $roomRepository->findBy(['id' => $id]),
+            'total_days' => $total_days,
+            'checkindate' => $dateCheckin,
+            'checkoutdate' => $dateCheckout,
+            'total_price' => $total_price
+        ]);
+
+    }
+
+        /**
+     * @Route("/result/checkindate={checkindate}&checkoutdate={checkoutdate}/view/{id}/book", name="final")
+     */
+    public function book($checkinDate, $checkoutDate, RoomRepository $roomRepository, Request $request, $id)  : Response
+    {
+        {
+                $user = $this->container->get('security.context')->getToken()->getUser();
+                $format = 'd-m-Y';
+                $room = $this->getDoctrine()->getRepository(Room::class)->find($id);
+                $user = $this->container->get('security.context')->getToken()->getUser();
+
+                $reservation = new Reservation();
+                $reservation->setCheckinDate(DateTime::createFromFormat($format, $checkinDate));
+                $reservation->setCheckoutDate(DateTime::createFromFormat($format, $checkoutDate));
+                $reservation->setRoom($room);
+                $reservation->setUser($user);
+                $reservation->setPrice(2);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($reservation);
+                $entityManager->flush();
+
+
+            return $this->render('make_reservation/final.html.twig', [
+                           ]);
+        }
+    }
 }
